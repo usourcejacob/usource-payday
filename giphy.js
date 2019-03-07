71b06cc4ba447711e810c48eb5ee2791719d7693
Jacob Hernandez <46951785+usourcejacob@users.noreply.github.com>
2019-03-07 15:06:38 +0800

    Updated site

---
+++ giphy.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    // Initiate gifLoop for set interval
+    var refresh;
+    // Duration count in seconds
+    const duration = 1000 * 10;
+    // Giphy API defaults
+    const giphy = {
+        baseURL: "https://api.giphy.com/v1/gifs/",
+        key: "dc6zaTOxFJmzC",
+        tag: "cash, money, payday",
+        type: "random",
+        rating: "pg-13"
+    };
+    // Target gif-wrap container
+    const $gif_wrap = $("#gif-wrap");
+    // Giphy API URL
+    let giphyURL = encodeURI(
+        giphy.baseURL +
+        giphy.type +
+        "?api_key=" +
+        giphy.key +
+        "&tag=" +
+        giphy.tag +
+        "&rating=" +
+        giphy.rating
+    );
+
+    // Call Giphy API and render data
+    var newGif = () => $.getJSON(giphyURL, json => renderGif(json.data));
+
+    // Display Gif in gif wrap container
+    var renderGif = _giphy => {
+        // Set gif as bg image
+        $gif_wrap.css({
+            "background-image": 'url("' + _giphy.image_original_url + '")',
+            "min-height" : 'auto',
+            "width" : '600px'
+        });
+
+        // Start duration countdown
+        refreshRate();
+    };
+
+    // Call for new gif after duration
+    var refreshRate = () => {
+        // Reset set intervals
+        clearInterval(refresh);
+        refresh = setInterval(function () {
+            // Call Giphy API for new gif
+            newGif();
+        }, duration);
+    };
+
+    // Call Giphy API for new gif
+    newGif();
+});
